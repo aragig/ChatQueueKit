eedaf6dd9715e4c3a9bce02b006f6620f6201964
Toshihiko Arai <araemonz@gmail.com>
2024-10-18 18:46:53 +0900

    Initial Commit

---
+++ ChatQueueKit/ChatQueueKit.h
@@ -0,0 +1,18 @@
+//
+//  ChatQueueKit.h
+//  ChatQueueKit
+//
+//  Created by Toshihiko Arai on 2024/10/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChatQueueKit.
+FOUNDATION_EXPORT double ChatQueueKitVersionNumber;
+
+//! Project version string for ChatQueueKit.
+FOUNDATION_EXPORT const unsigned char ChatQueueKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatQueueKit/PublicHeader.h>
+
+
